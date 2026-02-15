291cf7b7189ba209042b720fd7b7ef5d81ddc355
Skywalker690 <sanjoksiby0@gmail.com>
2026-02-15 14:50:18 +0530

    remove Meeting and Task controllers, DTOs, entities, and repositories

--- server/src/main/java/com/incial/crm/repository/MeetingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.incial.crm.repository;
-
-import com.incial.crm.entity.Meeting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeetingRepository extends JpaRepository<Meeting, Long> {
-}
